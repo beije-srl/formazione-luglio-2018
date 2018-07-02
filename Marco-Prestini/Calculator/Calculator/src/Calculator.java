2d0a570db612d34ec8cf3b0c01639c3b8a81cbdc
Lorenzo <lorenzo.ampri@gmail.com>
2018-07-02 14:19:48 +0200

    Aggiunto progetto calcolatrice

---
+++ Marco-Prestini/Calculator/Calculator/src/Calculator.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		int num1;
+		int num2;
+		String operation;
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Digita il primo numero");
+		num1 = input.nextInt();
+
+		System.out.println("Digita il secondo numero");
+		num2 = input.nextInt();
+
+		Scanner op = new Scanner(System.in);
+
+		System.out.println("Inserisci l'operatore");
+		operation = op.next();
+
+		switch (operation) {
+		case "+":
+			System.out.println("Risultato:" + (num1 + num2));
+			break;
+
+		case "-":
+			System.out.println("Risultato:" + (num1 - num2));
+			break;
+
+		case "*":
+			System.out.println("Risultato:" + (num1 * num2));
+			break;
+
+		case "/":
+			System.out.println("Risultato:" + (num1 / num2));
+			break;
+
+		case "^":
+			System.out.println("Risultato:" + Math.pow(num1, num2));
+			break;
+
+		case "r":
+			System.out.println("Risultato:" + Math.pow(num2, 1/num1));
+			break;
+
+		default:
+			System.out.println("Operazione sconosciuta");
+			break;
+		}
+
+	}
+
+}
